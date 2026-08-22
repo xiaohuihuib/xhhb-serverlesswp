535b4f4687eb910c322e3353514268586c414d29
xiaohuihuib <xiaohuihuib@users.noreply.github.com>
2026-08-22 13:04:22 +0000

    Automated WordPress version update

---
+++ wp/wp-content/plugins/wp-china-yes/Service/Base.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace WenPai\ChinaYes\Service;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Class Base
+ * 插件主服务
+ * @package WenPai\ChinaYes\Service
+ */
+class Base {
+
+	public function __construct() {
+		// 加速服务
+		new Super();
+		// 监控服务
+		new Monitor();
+		// 内存服务
+		new Memory();
+		// 更新服务
+		new Update();
+		if ( is_admin() ) {
+		// 设置服务
+		new Setting();
+		}
+	}
+}
