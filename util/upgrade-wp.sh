85035e2516752546a76acfb0cab4d733a29d727e
小辉辉b <3083869156@qq.com>
2026-08-22 16:20:12 +0800

    Remove obsolete plugin installation steps
    Removed installation steps for Amazon S3 and SQLite Database Integration plugins.

--- util/upgrade-wp.sh
+++ util/upgrade-wp.sh
@@ -31,14 +31,6 @@ cp ../wp/wp-content/mu-plugins/serverlesswp.php wordpress/wp-content/mu-plugins/
 cp ../wp/wp-content/mu-plugins/serverlesswp-stream-wrapper.php wordpress/wp-content/mu-plugins/
 cp -r ../wp/wp-content/mu-plugins/serverlesswp-stream-wrapper wordpress/wp-content/mu-plugins/
 
-wget https://downloads.wordpress.org/plugin/amazon-s3-and-cloudfront.zip
-unzip amazon-s3-and-cloudfront.zip
-mv amazon-s3-and-cloudfront wordpress/wp-content/plugins/
-
-git clone --depth 1 https://github.com/WordPress/sqlite-database-integration.git
-cp -rL sqlite-database-integration/packages/plugin-sqlite-database-integration wordpress/wp-content/plugins/sqlite-database-integration
-rm -rf sqlite-database-integration
-
 wget https://downloads.wordpress.org/plugin/tidb-compatibility.zip
 unzip tidb-compatibility
 mv tidb-compatibility wordpress/wp-content/plugins/
