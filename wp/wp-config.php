0346ed7de99274816616c881539e4bd4027000a3
xiaohuihuib <3083869156@qq.com>
2026-08-28 19:28:14 +0800

    删除多余代码禁用自动更新

--- wp/wp-config.php
+++ wp/wp-config.php
@@ -120,6 +120,11 @@ define('WP_HOME', 'https://' . $_SERVER['HTTP_HOST']);
 define('DISALLOW_FILE_EDIT', true );
 define('DISALLOW_FILE_MODS', true );
 
+// Disable WordPress automatic update checks; they trigger external requests
+// and write attempts that are slow and useless in a read-only serverless env.
+define('AUTOMATIC_UPDATER_DISABLED', true );
+define('WP_AUTO_UPDATE_CORE', false );
+
 define('WP_DEFAULT_THEME', 'argon');
 
 /* That's all, stop editing! Happy publishing. */
