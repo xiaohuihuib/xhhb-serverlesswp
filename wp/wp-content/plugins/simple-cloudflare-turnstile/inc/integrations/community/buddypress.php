ce1f348681ec9e8dee79245830ff08a2c8ac1868
xiaohuihuib <187079001+xiaohuihuib@users.noreply.github.com>
2026-09-05 04:38:47 +0000

    Update bundled plugins

--- wp/wp-content/plugins/simple-cloudflare-turnstile/inc/integrations/community/buddypress.php
+++ wp/wp-content/plugins/simple-cloudflare-turnstile/inc/integrations/community/buddypress.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 if ( ! defined( 'ABSPATH' ) ) {
     exit;
@@ -28,35 +27,4 @@ if(get_option('cfturnstile_bp_register')) {
 		}
 	}
 
-=======
-<?php
-if ( ! defined( 'ABSPATH' ) ) {
-    exit;
-}
-
-if(get_option('cfturnstile_bp_register')) {
-
-	// Get turnstile field: BuddyPress
-	add_action('bp_before_registration_submit_buttons','cfturnstile_field_bp_register');
-	function cfturnstile_field_bp_register() {
-    	cfturnstile_field_show('#buddypress #signup-form .submit', 'turnstileBPCallback', 'buddypress-register', '-bp-register');
-  	}
-
-	// BuddyPress Register Check
-	add_action('bp_signup_validate', 'cfturnstile_bp_register_check', 10, 1);
-	function cfturnstile_bp_register_check(){
-		if(!cfturnstile_whitelisted()) {
-			if ( 'POST' === $_SERVER['REQUEST_METHOD'] ) {
-				$check = cfturnstile_check();
-				$success = $check['success'];
-				if($success != true) {
-					wp_die( '<p><strong>' . esc_html__( 'ERROR:', 'simple-cloudflare-turnstile' ) . '</strong> ' . cfturnstile_failed_message() . '</p>', 'simple-cloudflare-turnstile', array( 'response'  => 403, 'back_link' => 1, ) );
-				}
-			} else {
-				wp_die( '<p><strong>' . esc_html__( 'ERROR:', 'simple-cloudflare-turnstile' ) . '</strong> ' . cfturnstile_failed_message() . '</p>', 'simple-cloudflare-turnstile', array( 'response'  => 403, 'back_link' => 1, ) );
-			}
-		}
-	}
-
->>>>>>> b611d588c17c8190405bdb439d52598f63e685e7
 }
--- wp/wp-content/plugins/simple-cloudflare-turnstile/inc/integrations/other/clean-login.php
+++ wp/wp-content/plugins/simple-cloudflare-turnstile/inc/integrations/other/clean-login.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 if(!defined('ABSPATH')) {
     exit;
@@ -6,13 +5,4 @@ if(!defined('ABSPATH')) {
 
 add_action('cleanlogin_after_login_form', 'cfturnstile_field_login');
 add_action('cleanlogin_after_register_form', 'cfturnstile_field_register');
-=======
-<?php
-if(!defined('ABSPATH')) {
-    exit;
-}
-
-add_action('cleanlogin_after_login_form', 'cfturnstile_field_login');
-add_action('cleanlogin_after_register_form', 'cfturnstile_field_register');
->>>>>>> b611d588c17c8190405bdb439d52598f63e685e7
 add_action('cleanlogin_after_resetpassword_form', 'cfturnstile_field_reset');
